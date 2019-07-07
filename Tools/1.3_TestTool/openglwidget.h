a07749cc85e2f6b8305ee48bf5115368c887fd9a
ZhengPengqiao <157510312@qq.com>
2019-07-08 02:35:14 +0800

    添加一个测试工具

---
+++ Tools/1.3_TestTool/openglwidget.h
@@ -0,0 +1,54 @@
+#ifndef OPENGLWIDGET_H
+#define OPENGLWIDGET_H
+
+#include <QWidget>
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLBuffer>
+#include <QBasicTimer>
+#include <QOpenGLVertexArrayObject>
+
+namespace Ui {
+class OpenGLWidget;
+}
+
+class OpenGLWidget : public QOpenGLWidget, protected QOpenGLFunctions
+{
+    Q_OBJECT
+
+public:
+    explicit OpenGLWidget(QWidget *parent = 0);
+    void timerEvent(QTimerEvent *e);
+    ~OpenGLWidget();
+    QOpenGLBuffer positionBuf;
+    QOpenGLBuffer colorBuf;
+    QOpenGLVertexArrayObject mObject;
+protected:
+    void initializeGL();
+    void paintGL();
+    void resizeGL(int w, int h);
+
+public slots:
+    void valueChange(int val);
+
+private:
+    QOpenGLShaderProgram *program;
+    GLuint programId;
+    GLuint matrixLocation;
+    GLuint vertexLocation;
+    GLuint clorLocation;
+    GLuint verVbo;
+    GLuint colorVbo;
+
+    QMatrix4x4 projection;
+    QMatrix4x4 matrix;
+
+    float angle;
+    QBasicTimer timer;
+    GLuint vCount;
+private:
+    Ui::OpenGLWidget *ui;
+};
+
+#endif // OPENGLWIDGET_H
